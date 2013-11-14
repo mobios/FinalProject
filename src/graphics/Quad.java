10163dd641cee4b36a04f3fdd7c96281e287865c
Jon Weldon <jweldon@mines.edu>
2013-11-14 15:39:33 -0700

    Quads able to draw, need to implement rendering engine

--- src/graphics/Quad.java
+++ src/graphics/Quad.java
@@ -4,7 +4,9 @@ import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
 public class Quad extends Model {
@@ -24,6 +26,14 @@ public class Quad extends Model {
 		int tempVBO = GL15.glGenBuffers();
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, tempVBO);
 		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, verticiesBuffer, GL15.GL_STATIC_DRAW);
+		GL20.glVertexAttribPointer(0,3, GL11.GL_FLOAT, false, 0, 0);
+		
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+		GL30.glBindVertexArray(0);
+		
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vBO);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL15.GL_STATIC_DRAW);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
 		
 	}
 
