20a8afc3904aa680485aee1550e321e1b955cdd3
Jon Weldon <jweldon@mines.edu>
2013-11-14 19:26:43 -0700

    Added "generic" utilities, such as rectangle and region

---
+++ src/util/Rectangle.java
@@ -0,0 +1,43 @@
+package util;
+
+public class Rectangle implements Region{
+	float x, y;
+	float width, height;
+	Origin origin;
+	
+	public float[] d3Vertices(){
+		@SuppressWarnings("unchecked")
+		float[] ret = new float[4*2];
+		if(origin == Origin.CENTER){
+			ret[0] = x-width/2;
+			ret[1] = y+height/2;
+			ret[2] = x-width/2;
+			ret[3] = y-height/2;
+			ret[4] = x+width/2;
+			ret[5] = y-height/2;
+			ret[6] = x+width/2;
+			ret[7] = y+height/2;
+			return ret;
+		}
+		
+		ret[0] = x;
+		ret[1] = y;
+		
+		ret[2] = x;
+		ret[3] = y-height;
+		
+		ret[4] = x+width;
+		ret[5] = y-height;
+		
+		ret[6] = x+width;
+		ret[7] = y;
+		return ret;
+	}
+
+	@Override
+	public boolean contains(float x, float y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/util/Region.java
@@ -0,0 +1,5 @@
+package util;
+
+public interface Region{
+	public boolean contains(float x, float y);
+}
