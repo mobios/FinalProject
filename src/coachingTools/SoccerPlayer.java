456509f5b9db63c19ff09c5bfed25d540f0822f5
Zac McClain <zamcclai@mines.edu>
2013-11-14 18:55:55 -0700

    some minor updates

--- src/coachingTools/SoccerPlayer.java
+++ src/coachingTools/SoccerPlayer.java
@@ -1,7 +1,10 @@
 package coachingTools;
 
 public class SoccerPlayer extends Player {
-
+	
+	public static enum SoccerArea {GOAL, PENALTY_AREA, PENALTY_ARC, GOAL_AREA, CENTER_CIRCLE, LEFT_HALF, RIGHT_HALF}
+	private SoccerArea region;
+	
 	public SoccerPlayer() {
 		super();
 		
