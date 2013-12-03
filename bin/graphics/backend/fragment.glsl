6ecc9922634a1f02bc9f3b423880e3c7cd32b1fb
Zac McClain <zamcclai@mines.edu>
2013-12-02 18:26:53 -0700

    added feild .png

---
+++ bin/graphics/backend/fragment.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+
+uniform sampler2D texelSample;
+
+in vec2 pass_uv;
+in vec4 pass_tint;
+
+out vec4 color;
+
+void main(){
+	color = texture2D(texelSample, pass_uv) * pass_tint;
+	//color = pass_tint;
+}
