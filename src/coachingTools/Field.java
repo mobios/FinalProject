bda81a5dec10c381c85c16c509b6a901bc9b8dda
Jon Weldon <jweldon@mines.edu>
2013-12-03 01:59:02 -0700

    theoretical overhaul of player move

--- src/coachingTools/Field.java
+++ src/coachingTools/Field.java
@@ -4,10 +4,22 @@ import util.Rectangle;
 import graphics.frontend.BackgroundImage;
 
 public class Field extends BackgroundImage {
+	public static final float x = -.15f;
+	public static final float y = .0f;
+	public static final float width = 1.7f;
+	public static final float height = 2.0f;
+	
+	public static final float top = y+height/2;
+	public static final float bottom = y-height/2;
+	public static final float left = x-width/2;
+	public static final float right = x+width/2;
+	
+	
+	
 	
 	public Field() {
 		//creates a rectangle for the backgroundImage and imposes a .png image on it to create the field
-		super(new Rectangle(-.15f, .0f, 1.7f, 2.0f), "resources/field.png");
+		super(new Rectangle(x,y,width,height), "resources/field.png");
 	}
 	
 }
